29ab5685f8f76405043cebc5d0bb3733cf261979
RS0048 <sokolovrmn@gmail.com>
2024-01-24 12:43:15 +0300

    fix: Fixed bags 05, 06

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,37 +1,118 @@
 const fs = require('fs');
 const path = require('path');
 
-// deleted old bundle.css
-fs.unlink(path.join(__dirname, 'project-dist', 'bundle.css'), (genError) => {
-    if (genError) {
-      console.log('The file has not been created yet', genError);
-    }
-});
 
-// create new bundle.css and add all .css files
-fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (err, files) => {
-    if (err) {
-      console.error('Error reading', err.message);
-      return;
-    }
-    
-    files.forEach((element) => {
-        if (element.isFile()) {
-            const infoOfFile = path.parse(path.join(__dirname, 'styles', element.name));
-            const extOfFile = infoOfFile.ext;
-            if (extOfFile === '.css') {
+const deleteFile = (path) => {
+    return new Promise((resolve, reject) => {
+        fs.unlink(path, (err) => {
 
-                fs.readFile(path.join(__dirname, 'styles', element.name), 'utf8', (err, data) => {
-                    if (err) throw err;
+            console.log('delete')
+            resolve();
+        })
+    })
+}
 
-                    fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), (data + '\n\n'), (err2) => {
-                        if (err2) {
-                            console.log(err2);
-                        }
-                    });
-                });
+
+/*
+const readStyles = (pathTo) => {
+    return new Promise((resolve, reject) => {
+        console.log(pathTo)
+        fs.readdir(pathTo, { withFileTypes: true }, (err, files) => {
+            if (err) {
+                reject(err);
             }
-        }
-    });
+            const arrayOfStyles = []
+            Promise.all(
+                files.map((element) => {
+                   
+                    return new Promise((resolve2, reject2) => {
+                        if (err) {
+                            reject2(err);
+                        }
+                                fs.readFile(path.join(__dirname, 'styles', element.name), (err, data) => {
+                                    if (err) throw err;
+                                    
+                                    const infoOfFile = path.parse(path.join(__dirname, 'styles', element.name));
+                                    
+                                    const stylesInfo = {};
+                                    stylesInfo.name = infoOfFile.name;
+                                    stylesInfo.html = data.toString();
+                                    arrayOfStyles.push(stylesInfo);
+                                    resolve2();
+                                    
+                                });
+                            
+                        
+                        
+                    });
+                })
+            )
+            console.log(arrayOfStyles)
+            .then(() => resolve(arrayOfStyles)); 
+        });
+    })
+}
 
-});
+*/
+
+const readStyles = (pathTo) => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(pathTo, { withFileTypes: true }, (err, files) => {
+            if (err) {
+                reject(err);
+            }
+            const arrayOfComponents = []
+            const styleFiles = files.filter((file) => path.extname(file.name) === '.css' && file.isFile());
+            Promise.all(
+                styleFiles.map((element) => {
+                return new Promise((resolve2, reject2) => {
+                    if (err) {
+                        reject2(err);
+                    }
+
+                    fs.readFile(path.join(__dirname, 'styles', element.name), (err, data) => {
+                            if (err) throw err;
+
+                            const infoOfFile = path.parse(path.join(__dirname, 'styles', element.name));
+
+                            const componentInfo = {};
+                            componentInfo.name = infoOfFile.name;
+                            componentInfo.html = data;
+                            arrayOfComponents.push(componentInfo);
+                            resolve2();
+
+                        });
+                    });
+
+                })
+            )
+            .then(() => resolve(arrayOfComponents)); 
+        });
+    })
+}
+
+const appendStyles = (path, ArrStyles) => {
+    return new Promise((resolve, reject) => {
+        ArrStyles.forEach((file) => {
+            fs.appendFile(path, file.html, (err) => {
+                if (err) {
+                    reject(err);
+                }
+                console.log('append styles')
+                resolve();
+            })
+        })
+    })
+}
+
+
+
+const pathToStyles = path.join(__dirname, 'styles')
+const pathToStyle = path.join(__dirname, 'project-dist', 'bundle.css')
+
+deleteFile(pathToStyle)
+
+.then(() => readStyles(pathToStyles))
+.then((arrayOfStyles) => appendStyles(pathToStyle, arrayOfStyles))
+
+
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,28 +2,20 @@ const fs = require('fs');
 const path = require('path');
 
 
-//create folder project-dist
-fs.mkdir(path.join(__dirname, 'project-dist'), () => {
-    return;
-});
+//create folder 
 
-
-// deleted old index.html
-fs.unlink(path.join(__dirname, 'project-dist', 'index.html'), (genError) => {
-    if (genError) {
-      console.log('The file has not been created yet', genError);
-    }
-});
-
-fs.readFile(path.join(__dirname, 'template.html'), 'utf8', (err, textTemplate) => {
-    if (err) throw err;
-
-    fs.appendFile(path.join(__dirname, 'project-dist', 'index.html'), textTemplate, (err) => {
-        if (err) {
-            console.log(err);
-        }
-    });
-});
+const createFolders = () => {
+    return new Promise((resolve, reject) => {
+        fs.mkdir(path.join(__dirname, 'project-dist'), () => {
+            return;
+        });
+        fs.mkdir(path.join(__dirname, 'project-dist', 'assets'), () => {
+            return;
+        });
+        resolve();
+        console.log('create folders');
+    })
+}
 
 
 
@@ -31,57 +23,132 @@ fs.readFile(path.join(__dirname, 'template.html'), 'utf8', (err, textTemplate) =
 
 
 
+const readFile = (path) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(data);
+            console.log('read');
+        })
+    })
+}
 
-fs.readdir(path.join(__dirname, 'components'), { withFileTypes: true }, (err, files) => {
-    if (err) {
-      console.error('Error reading', err.message);
-      return;
-    }
+const writeFile = (path, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(path, data, (err) => {
+            if (err) {
+                reject(err);
+            }
+            console.log('write')
+            resolve();
+        })
+    })
+}
 
-    fs.readFile(path.join(__dirname, 'template.html'), 'utf8', (err, textTemplate) => {
-        if (err) throw err;
-        files.forEach((element) => {
-            const infoOfFile = path.parse(path.join(__dirname, 'components', element.name));
-            const nameOfFiles = infoOfFile.name;
+const deleteFile = (path) => {
+    return new Promise((resolve, reject) => {
+        fs.unlink(path, (err) => {
 
-            
-             fs.readFile(path.join(__dirname, 'components', element.name), 'utf8', (err, textFromComponent) => {
-                if (err) throw err;
-                textTemplate = textTemplate.replace(('{{' + nameOfFiles + '}}'), textFromComponent);
-                
+            console.log('delete')
+            resolve();
+        })
+    })
+}
 
-                 fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), textTemplate, (err) => {
+const appendFile = (path, data) => {
+    return new Promise((resolve, reject) => {
+        fs.appendFile(path, data, (err) => {
+            if (err) {
+                reject(err);
+            }
+            console.log('append')
+            resolve();
+        })
+    })
+}
+
+const readComponents = (pathTo) => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(pathTo, { withFileTypes: true }, (err, files) => {
+            if (err) {
+                reject(err);
+            }
+            const arrayOfComponents = []
+           Promise.all(
+            files.map((element) => {
+                return new Promise((resolve2, reject2) => {
                     if (err) {
-                        console.log(err);
+                        reject2(err);
                     }
-                    
+
+                    fs.readFile(path.join(__dirname, 'components', element.name), (err, data) => {
+                            if (err) throw err;
+
+                            const infoOfFile = path.parse(path.join(__dirname, 'components', element.name));
+
+                            const componentInfo = {};
+                            componentInfo.name = infoOfFile.name;
+                            componentInfo.html = data;
+                            arrayOfComponents.push(componentInfo);
+                            resolve2();
+
+                        });
+                    });
+
                 })
+            )
+            .then(() => resolve(arrayOfComponents)); 
+        });
+    })
+}
 
 
+const replaceComponent = (textFromComponent) => {
+    return new Promise ((resolve, reject) => {
+        readFile(pathToIndex)
+        .then ((data) => {
+            let textTemplate = data.toString();
+            textFromComponent.forEach((component) => {
+                textTemplate = textTemplate.replace(('{{' + component.name + '}}'), component.html);
             })
+            console.log('replace')
+            resolve(textTemplate);
         })
 
+    })
+}
 
 
+const deleteFolder = (pathTo) => {
+    return new Promise((resolve, reject) => {
+        fs.rm(pathTo, { recursive: true }, (err) => {
+            resolve();
+            console.log('delete folder')
+        });
     });
-
-});
-
+}
 
 
+const pathToTemplate = path.join(__dirname, 'template.html')
+const pathToIndex = path.join(__dirname, 'project-dist', 'index.html')
+const pathToComponents = path.join(__dirname, 'components')
+const pathToProject = path.join(__dirname, 'project-dist')
+const pathToStyles = path.join(__dirname, 'styles')
+const pathToStyle = path.join(__dirname, 'project-dist', 'style.css')
 
+deleteFolder(pathToProject)
 
-
-
-
-
-
-
-
-
-
-
-
+.then(() => createFolders())
+.then(() => readFile(pathToTemplate))
+.then((data) => appendFile(pathToIndex, data))
+.then(() => readComponents(pathToComponents))
+.then((arrayOfComponents) => replaceComponent(arrayOfComponents))
+.then((textTemplate) => writeFile(pathToIndex, textTemplate))
+.then(() => readStyles(pathToStyles))
+.then((arrayOfStyles) => appendStyles(pathToStyle, arrayOfStyles))
+.then(() => copyAssets())
 
 
 
@@ -114,14 +181,13 @@ fs.readdir(path.join(__dirname, 'components'), { withFileTypes: true }, (err, fi
 */
 
 // deleted old style.css
-fs.unlink(path.join(__dirname, 'project-dist', 'style.css'), (genError) => {
-    if (genError) {
-      console.log('The file has not been created yet', genError);
-    }
-});
+//fs.unlink(path.join(__dirname, 'project-dist', 'style.css'), (genError) => {
+//
+//});
 
 
 // create new style.css and add all .css files
+/*
 fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (err, files) => {
     if (err) {
       console.error('Error reading', err.message);
@@ -148,31 +214,117 @@ fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (err, files)
     });
 
 });
+*/
+/*
+const readStyles = (pathTo) => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(pathTo, { withFileTypes: true }, (err, files) => {
+            if (err) {
+                reject(err);
+            }
+            const arrayOfStyles = []
+            Promise.all(
+            files.map((element) => {
+                return new Promise((resolve2, reject2) => {
+                    if (err) {
+                        reject2(err);
+                    }
+
+                    fs.readFile(path.join(__dirname, 'styles', element.name), (err, data) => {
+                            if (err) throw err;
+
+                            const infoOfFile = path.parse(path.join(__dirname, 'styles', element.name));
+
+                            const stylesInfo = {};
+                            stylesInfo.name = infoOfFile.name;
+                            stylesInfo.html = data;
+                            arrayOfStyles.push(stylesInfo);
+                            resolve2();
+
+                        });
+                    });
+
+                })
+            )
+            .then(() => resolve(arrayOfStyles)); 
+        });
+    })
+}
 
 
-// create new folder - assets
-fs.mkdir(path.join(__dirname, 'project-dist', 'assets'), () => {
-    return;
-});
 
 
-// delete all files from folder assets
-/*fs.readdir(path.join(__dirname, 'project-dist', 'assets'), { withFileTypes: true }, (err, files) => {
-    if (err) {
-      console.error('Error reading', err.message);
-      return;
-    }
-    files.forEach((element) => {
-        if (element.isFile()) {
-            fs.unlink(path.join(__dirname, 'project-dist', 'assets', element.name), (genError) => {
-                if (genError) {
-                  console.log("Operation Failed: ", genError);
+const appendStyles = (path, ArrStyles) => {
+    return new Promise((resolve, reject) => {
+        ArrStyles.forEach((file) => {
+            fs.appendFile(path, file.html, (err) => {
+                if (err) {
+                    reject(err);
                 }
-            });
-        }
-    });
-});*/
+                console.log('append styles')
+                resolve();
+            })
+        })
+    })
+}
 
+*/
+
+const readStyles = (pathTo) => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(pathTo, { withFileTypes: true }, (err, files) => {
+            if (err) {
+                reject(err);
+            }
+            const arrayOfComponents = []
+            const styleFiles = files.filter((file) => path.extname(file.name) === '.css' && file.isFile());
+            Promise.all(
+                styleFiles.map((element) => {
+                return new Promise((resolve2, reject2) => {
+                    if (err) {
+                        reject2(err);
+                    }
+
+                    fs.readFile(path.join(__dirname, 'styles', element.name), (err, data) => {
+                            if (err) throw err;
+
+                            const infoOfFile = path.parse(path.join(__dirname, 'styles', element.name));
+
+                            const componentInfo = {};
+                            componentInfo.name = infoOfFile.name;
+                            componentInfo.html = data;
+                            arrayOfComponents.push(componentInfo);
+                            resolve2();
+
+                        });
+                    });
+
+                })
+            )
+            .then(() => resolve(arrayOfComponents)); 
+        });
+    })
+}
+
+const appendStyles = (path, ArrStyles) => {
+    return new Promise((resolve, reject) => {
+        ArrStyles.forEach((file) => {
+            fs.appendFile(path, file.html, (err) => {
+                if (err) {
+                    reject(err);
+                }
+                console.log('append styles')
+                resolve();
+            })
+        })
+    })
+}
+
+
+
+
+
+/*
 
 fs.readdir(path.join(__dirname, 'project-dist', 'assets'), { withFileTypes: true }, (err, files) => {
     if (err) {
@@ -208,44 +360,54 @@ fs.readdir(path.join(__dirname, 'project-dist', 'assets'), { withFileTypes: true
     })
 });
 
-
+*/
 
 // copy files assets
-fs.readdir(path.join(__dirname, 'assets'), { withFileTypes: true }, (err, files) => {
-    if (err) {
-      console.error('Error reading', err.message);
-      return;
-    }
-    files.forEach((element) => {
-        if (element.isDirectory()) {
-            fs.mkdir(path.join(__dirname, 'project-dist', 'assets', element.name), () => {
-                return;
-            });
 
-            fs.readdir(path.join(__dirname, 'assets', element.name), { withFileTypes: true }, (err, files) => {
-                if (err) {
-                    console.error('Error reading', err.message);
+const copyAssets = () => {
+    fs.readdir(path.join(__dirname, 'assets'), { withFileTypes: true }, (err, files) => {
+        if (err) {
+          console.error('Error reading', err.message);
+          return;
+        }
+        console.log('copy')
+        files.forEach((element) => {
+            if (element.isDirectory()) {
+                fs.mkdir(path.join(__dirname, 'project-dist', 'assets', element.name), () => {
                     return;
-                }
-                files.forEach((file) => {
-                    if (file.isFile()) {
-                        fs.copyFile(path.join(__dirname, 'assets', element.name, file.name), path.join(__dirname, 'project-dist', 'assets', element.name, file.name), (genError) => {
-                            if (genError) {
-                                console.log("Operation Failed: ", genError);
-                            }
-                        });
+                });
+    
+                fs.readdir(path.join(__dirname, 'assets', element.name), { withFileTypes: true }, (err, files) => {
+                    if (err) {
+                        console.error('Error reading', err.message);
+                        return;
                     }
+                    files.forEach((file) => {
+                        if (file.isFile()) {
+                            fs.copyFile(path.join(__dirname, 'assets', element.name, file.name), path.join(__dirname, 'project-dist', 'assets', element.name, file.name), (genError) => {
+                                if (genError) {
+                                    console.log("Operation Failed: ", genError);
+                                }
+                            });
+                        }
+                    })
                 })
-            })
-        }
+            }
+    
+            if (element.isFile()) {
+                fs.copyFile(path.join(__dirname, 'assets', element.name), path.join(__dirname, 'project-dist', 'assets', element.name), (genError) => {
+                    if (genError) {
+                        console.log("Operation Failed: ", genError);
+                    }
+                });
+            }
+        })
+    });
+    
+}
 
-        if (element.isFile()) {
-            fs.copyFile(path.join(__dirname, 'assets', element.name), path.join(__dirname, 'project-dist', 'assets', element.name), (genError) => {
-                if (genError) {
-                    console.log("Operation Failed: ", genError);
-                }
-            });
-        }
-    })
-});
+
+
+
+
 
